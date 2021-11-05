31d6299dadc488b34d29d5fedb0ca58f5709516a
kiran <kletherwala@gmail.com>
2021-11-05 12:37:40 -0500

    complete codegrade

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from "./components/Character";
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [starwars, setStarwars] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      console.log(res.data);
+      setStarwars(res.data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }, [])
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {starwars.map((info, i) => {
+        return <Character key={i} name={info.name} birthyear={info.birth_year}/>
+      })}
     </div>
   );
-}
+};
+
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
-// Write your Character component here
+// // Write your Character component here
+// import React, { useState, useEffect } from 'react';
+// import styled from 'styled-components';
+
+
+// co
+
+import React from 'react';
+import styled from "styled-components";
+
+const CharacterDiv = styled.div`
+    border: 1px black solid;
+    width: 50%;
+    margin: 0 auto;
+    font-family: serif;
+    font-size: 20px;
+`;
+
+function Character(character) {
+
+    return (
+        <CharacterDiv>
+            <p>{character.name}<br />
+            Year of birth: {character.birthYear}</p>
+        </CharacterDiv>
+
+    )
+}
+
+export default Character
